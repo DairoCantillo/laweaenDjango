05a2de4942f25918a2e8b8bf28d7de7ba17f8087
Dairo Cantillo <cantillodairo62@gmail.com>
2018-04-11 10:59:07 -0500

    la base de datos ahora carga imagenes

--- productos/models.py
+++ productos/models.py
@@ -7,11 +7,12 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=30)
     descripcion = models.CharField(max_length=255)
     categoria = models.CharField(max_length=40)
-    precio = models.DecimalField(max_digits=7, decimal_places=2)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
     formadepago = models.CharField(max_length=30)
+    imagen = models.ImageField(blank=True)
 
     def __str__(self):
-        return self.name
+        return self.nombre
 
     class Meta:
         ordering = ('id',)

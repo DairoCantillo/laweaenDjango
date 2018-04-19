5b1decd3565bf51d67bae6916715d1a0c5554158
Dairo Cantillo <cantillodairo62@gmail.com>
2018-04-18 23:59:26 -0500

    listos

--- myappweb/settings.py
+++ myappweb/settings.py
@@ -82,12 +82,15 @@ WSGI_APPLICATION = 'myappweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
+
+
+import dj_database_url  
+db_from_env = dj_database_url.config(conn_max_age=500)  
+DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 
 # Password validation
